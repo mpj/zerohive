21d1c6fd5dae862b6fad747c8a45b700be9b3239
Mattias Petter Johansson <mpj@spotify.com>
2013-07-18 19:11:09 +0200

    Added basic locking and evaluation

--- public/viewmodels/case.js
+++ public/viewmodels/case.js
@@ -9,6 +9,14 @@ if (typeof(ZeroHive) === 'undefined') ZeroHive = {};
 
     var sandbox = Sandbox.facade();
 
+    var lockedResult = ko.observable(null);
+
+    self.locked = ko.observable(false);
+    self.locked.subscribe(function(l) {
+      if (l) lockedResult(self.result());
+      else lockedResult(null);
+    });
+
     self.source = ko.observable(null);
 
     self.functionArguments = argumentNames.map(function(a) {
@@ -18,15 +26,34 @@ if (typeof(ZeroHive) === 'undefined') ZeroHive = {};
       };
     });
 
-    ko.computed(function() {
-      if (self.source() === null) return;
-      var argumentValues = self.functionArguments.map(function(a) {
+    var argumentValues = ko.computed(function() {
+      return self.functionArguments.map(function(a) {
         return a.value();
       });
-      sandbox.execute(self.source(), argumentValues);
     });
 
-    self.result = sandbox.result;
+    argumentValues.subscribe(function() { self.locked(false); });
+
+    ko.computed(function() {
+      if (self.source() === null) return;
+      sandbox.execute(self.source(), argumentValues());
+    });
+
+    var pass = ko.computed(function() {
+      if (lockedResult() === null) return null;
+      return _.isEqual(sandbox.result(), lockedResult());
+    });
+
+    self.result = ko.computed(function() {
+      if (pass() || pass() === null)
+        return sandbox.result();
+      return sandbox.result()+ '(Expected ' + lockedResult() + ')';
+    });
+
+    self.resultClass = ko.computed(function() {
+      if (pass() === null) return '';
+      return pass() ? 'pass' : 'fail';
+    });
 
     return self;
   };
