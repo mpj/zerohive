0efa7cc43ab7215a56121c1665814b6300958f5c
Mattias Petter Johansson <mpj@spotify.com>
2013-07-20 11:29:57 +0200

    Made sure code mirrors are disposed

--- public/binding-handlers/code-mirror.js
+++ public/binding-handlers/code-mirror.js
@@ -1,20 +1,30 @@
 ko.bindingHandlers.codeMirror = {
     init: function(element, valueAccessor) {
       var viewModel = ko.unwrap(valueAccessor());
-      var codeMirror = CodeMirror(function(elt) {
-        element.parentNode.replaceChild(elt, element);
-      }, {
+
+      var options = {
         mode:  'javascript',
         tabSize: 2
-      });
+      };
 
-      codeMirror.on('change', function() {
-        viewModel.value(codeMirror.getValue());
-      });
+      var codeMirror = CodeMirror.fromTextArea(element, options);
 
+      // When codeMirror input chnages, transfer value to value() observable
+      // on the viewMode.
+      var onChange = function() { viewModel.value(codeMirror.getValue()); };
+      codeMirror.on('change', onChange);
+
+      // The inverse of onChange - transfer viewModel value changes to codeMirror
       ko.computed(function() {
+        // Ignore if same, or we'd have infinite loops of changes
         if (viewModel.value() === codeMirror.getValue()) return;
         codeMirror.setValue(viewModel.value());
+      }).disposeWhenNodeIsRemoved(element);
+
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+        var wrapper = codeMirror.getWrapperElement();
+        wrapper.parentNode.removeChild(wrapper);
+        codeMirror.off('change', onChange);
       });
     }
 };
