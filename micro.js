fcb18753f5a32b0f603d865038a4ad4b29636adf
Mattias Petter Johansson <mpj@spotify.com>
2013-07-20 21:06:04 +0200

    more oops

--- micro.js
+++ micro.js
@@ -12,6 +12,6 @@ var app = connect()
     res.end(index);
   });
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 http.createServer(app).listen(port);
 console.log("Listening on port", port)
