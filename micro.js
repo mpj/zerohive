0033ca7ba78834db76cbbf465d3252dca89e091a
Mattias Petter Johansson <mpj@spotify.com>
2013-07-20 20:54:55 +0200

    We not need no stinking express.

---
+++ micro.js
@@ -0,0 +1,17 @@
+var connect = require('connect')
+  , http = require('http')
+  , fs = require('fs');
+
+ var index = fs.readFileSync('views/index.html');
+
+var app = connect()
+  .use(connect.favicon())
+  .use(connect.static('public'))
+  .use(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(index);
+  });
+
+var port = 3000;
+http.createServer(app).listen(port);
+console.log("Listening on port", port)
