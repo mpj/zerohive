c90692d94ed2f1a47c54ed9e94e97de7c7dbe276
Mattias Petter Johansson <mpj@spotify.com>
2013-08-04 16:46:33 +0200

    Adding cache to production

--- micro.js
+++ micro.js
@@ -2,12 +2,18 @@ var connect = require('connect')
   , http = require('http')
   , fs = require('fs');
 
- var index = fs.readFileSync('views/index.html');
+
+
+var cacheAge = 0;
+if (process.env.NODE_ENV === 'production') {
+  cacheAge = 60 * 60 * 1000;
+}
 
 var app = connect()
   .use(connect.favicon())
-  .use(connect.static('public'))
+  .use(connect.static('public', { maxAge: cacheAge }))
   .use(function(req, res) {
+    var index = fs.readFileSync('views/index.html');
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(index);
   });
